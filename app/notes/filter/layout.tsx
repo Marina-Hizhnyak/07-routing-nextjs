d29474b49b4599a78037c305b108e960d55810f2
Marina-Hizhnyak <maryfadeeva1208@gmail.com>
2025-08-29 13:44:19 +0200

    correction modal

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -3,17 +3,14 @@ import css from './layout.module.css';
 export default function FilterLayout({
   children,
   sidebar,
-  modal,
 }: {
   children: React.ReactNode;
   sidebar: React.ReactNode;
-  modal: React.ReactNode;
 }) {
   return (
     <div className={css.container}>
       <aside className={css.sidebar}>{sidebar}</aside>
       <main className={css.notesWrapper}>{children}</main>
-      {modal}
     </div>
   );
 }
